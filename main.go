0db7f7961f5ca48aa43a427e83947fb425e01334
Alexey Rumyantsev <dev@bpdu.ru>
2024-08-29 16:42:12 +0300

    Add a usage tip in case of no args

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const HELP_MESSAGE = `
+Usage: go run GetNetByIP [OPTION]... IP_ADDRESS
+
+Get network ID and some metadata by requested IP address.
+
+Mandatory arguments to long options are mandatory for short options too.
+
+Options:
+
+  -i, --info                  Returns a network ID.
+
+`
 
 func main() {
-	fmt.Println("Network Id is 0.0.0.0/0")
+
+	if len(os.Args) == 1 {
+		fmt.Print(HELP_MESSAGE)
+	}
 }
